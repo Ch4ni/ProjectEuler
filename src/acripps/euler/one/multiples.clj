3a29780fdf8b371b04e74f9d4fc20fb241ecf804
Aaron Cripps <acripps@gmail.com>
2017-05-01 23:38:52 -0400

    add FIXME comment

--- src/acripps/euler/one/multiples.clj
+++ src/acripps/euler/one/multiples.clj
@@ -4,4 +4,5 @@
 (defn get-multiples
   "given a collection, return the list that are multiples of x"
   [x coll]
+  ;; FIXME: This does not do what the description says ... working out how to get midje to pick up the new code
   (divides? x 10))
