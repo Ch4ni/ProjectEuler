cd54450f04f04fdd9fdf295d7be1c5695d914fda
Aaron Cripps <acripps@gmail.com>
2017-05-01 23:30:33 -0400

    initial commit

---
+++ test/euler/one/multiple_test.clj
@@ -0,0 +1,7 @@
+(ns euler.one.multiple_test
+  (:use midje.sweet)
+  (:require [acripps.euler.one.multiples :as multiples]))
+
+(facts "About get-multiples"
+       (fact "multiples of 3 less than 10 are [3 6 9]"
+             (multiples/get-multiples 3 (range 1 10) => [3 6 9])))
---
+++ test/euler/two/fib_test.clj
@@ -0,0 +1,3 @@
+(ns euler.two.fib_test
+  (:use midje.sweet)
+  (:require [acripps.euler.two.fib :as fib]))
