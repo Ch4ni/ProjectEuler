b959ab849221778396632a32c51dcb832a89b2d8
Aaron Cripps <acripps@gmail.com>
2017-05-02 00:09:57 -0400

    complete problem one

--- src/acripps/euler/one/multiples.clj
+++ src/acripps/euler/one/multiples.clj
@@ -3,5 +3,14 @@
 
 (defn get-multiples
   "given a collection, return the collation that are multiples of x"
-  [x coll]
-  (filter (partial divides? x) coll))
+  ([x coll]
+   (filter (partial divides? x) coll))
+  ([x y coll]
+   (into [] (sort (into (into #{} (get-multiples x coll)) (get-multiples y coll))))))
+
+
+(defn sum-multiples
+  ([x max]
+   (reduce + (get-multiples x (range 1 max))))
+  ([x y max]
+   (reduce + (get-multiples x y (range 1 max)))))
