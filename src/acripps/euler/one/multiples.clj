c53b480bb01954cbcf85224864635a5376dbdb80
Aaron Cripps <acripps@gmail.com>
2017-05-01 23:55:44 -0400

    properly implement get-multiples

--- src/acripps/euler/one/multiples.clj
+++ src/acripps/euler/one/multiples.clj
@@ -2,7 +2,6 @@
   (:require [acripps.euler.core :refer [divides?]]))
 
 (defn get-multiples
-  "given a collection, return the list that are multiples of x"
+  "given a collection, return the collation that are multiples of x"
   [x coll]
-  ;; FIXME: This does not do what the description says ... working out how to get midje to pick up the new code
-  (divides? x 10))
+  (filter (partial divides? x) coll))
