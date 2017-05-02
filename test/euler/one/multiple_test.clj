e67fc48650d176cfdc40135f11353956d190cc3d
Aaron Cripps <acripps@gmail.com>
2017-05-01 23:55:19 -0400

    fix scoping issue in test

--- test/euler/one/multiple_test.clj
+++ test/euler/one/multiple_test.clj
@@ -4,4 +4,4 @@
 
 (facts "About get-multiples"
        (fact "multiples of 3 less than 10 are [3 6 9]"
-             (multiples/get-multiples 3 (range 1 10) => [3 6 9])))
+             (multiples/get-multiples 3 (into [] (range 1 10))) => [3 6 9]))
