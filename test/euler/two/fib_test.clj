6419c790584dcf1fb6ce57cff8e4ecca89e22eaf
Aaron Cripps <aaron.cripps@cenx.com>
2017-05-02 22:48:50 -0400

    implement and test addition of fibonacci sequence terms

--- test/euler/two/fib_test.clj
+++ test/euler/two/fib_test.clj
@@ -1,3 +1,20 @@
 (ns euler.two.fib_test
   (:use midje.sweet)
   (:require [acripps.euler.two.fib :as fib]))
+
+(facts "About the fibonnaci sequence"
+       (fact "the first term is 1"
+             (take 1 (fib/fibonacci)) => '(1))
+       (fact "the first three terms are (1 1 2)"
+             (take 3 (fib/fibonacci)) => '[1 1 2])
+       (fact "the first 11 terms are (1 1 2 3 5 8 13 21 34 55 89)"
+             (take 11 (fib/fibonacci)) => '(1 1 2 3 5 8 13 21 34 55 89)))
+
+
+(facts "About fibonacci sums"
+       (fact "all the even numbers below 100 sum to 44"
+             (fib/sum even? (partial <= 100)) => 44)
+       (fact "all the odd numbers below 100 sum to 188"
+             (fib/sum (complement even?) (partial <= 100)) => 188)
+       (fact "the sum of all even numbered terms below 4 million is 4613732"
+             (fib/sum even? (partial <= 4000000)) => 4613732))
