5373981bce8babd31eea6926818aec775dd3b1c9
Aaron Cripps <acripps@gmail.com>
2017-05-04 00:01:13 -0400

    implement palindrome detection

--- src/acripps/euler/four/palindrome.clj
+++ src/acripps/euler/four/palindrome.clj
@@ -1,1 +1,9 @@
-(ns acripps.euler.four.palindrome)
+(ns acripps.euler.four.palindrome
+  (:require [clojure.string :as s]))
+
+(defn palindrome?
+  "takes a string as an argument and determines whether or not it is a palindrome"
+  [x]
+  (let [forward (s/replace x #" " "")
+        backward (s/reverse forward)]
+    (= forward backward)))
--- test/euler/four/palindrome_test.clj
+++ test/euler/four/palindrome_test.clj
@@ -7,3 +7,15 @@
   Find the largest palindrome made from the product of two 3-digit numbers.")
 
 (comment "I think my approach should be to start at the largest 3-digit numbers, check the product, decrement one, check the product, then decrement the other and check the product. Repeat as required.")
+
+(facts "About palindrome?"
+       (fact "the empty string is a palindrome"
+             (p/palindrome? "") => true)
+       (fact "the string \" \" is a palindrome"
+             (p/palindrome? " ") => true)
+       (fact "the string \"aoa\" is a palindrome"
+             (p/palindrome? "aoa") => true)
+       (fact "the string \"ao\" is not a palindrome"
+             (p/palindrome? "ao") => false)
+       (fact "the string \"a man a plan a canal panama\" is a palindrome"
+             (p/palindrome? "a man a plan a canal panama") => true))
