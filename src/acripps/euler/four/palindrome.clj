f2e706bc5cb460ec0a7a205df11b6d225f4db01d
Aaron Cripps <acripps@gmail.com>
2017-05-04 00:02:14 -0400

    add comment describing rationale for dirty hack

--- src/acripps/euler/four/palindrome.clj
+++ src/acripps/euler/four/palindrome.clj
@@ -4,6 +4,7 @@
 (defn palindrome?
   "takes a string as an argument and determines whether or not it is a palindrome"
   [x]
+  ;; NOTE: replacing " " is a hack, because #"\\S+" doesn't appear to work correctly
   (let [forward (s/replace x #" " "")
         backward (s/reverse forward)]
     (= forward backward)))
