5b6b3ded69d34046cdb1fad51049bf28906b569c
Aaron Cripps <aaron.cripps@cenx.com>
2017-05-07 01:34:55 -0400

    add positive and negative checks.

--- test/euler/core_test.clj
+++ test/euler/core_test.clj
@@ -7,3 +7,19 @@
              (c/divides? 1 2) => true)
        (fact "returns false when one number does not divide the other"
              (c/divides? 2 1) => false))
+
+(facts "about positive?"
+       (fact "0 is positive"
+             (c/positive? 0) => true)
+       (fact "1 is positive"
+             (c/positive? 1) => true)
+       (fact "-1 is not positive"
+             (c/positive? -1) => false))
+
+(facts "about negative?"
+       (fact "0 is not negative"
+             (c/negative? 0) => false)
+       (fact "1 is not negative"
+             (c/negative? 1) => false)
+       (fact "-1 is negative"
+             (c/negative -1) => true))
